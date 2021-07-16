6988ca351eea0fac9ab279c75612b4372b9b14f5
Pratik Gaikwad <pratikgaikwad551@gmail.com>
2021-07-16 13:40:06 +0530

    added money spent and money left feature

--- index.js
+++ index.js
@@ -5,14 +5,34 @@ var modalItem = document.getElementById("myModal-item");
 var span = document.getElementsByClassName("close")[0];
 var SalaryInput=document.getElementById("salary-input");
 var ItemInput=document.getElementById("item-input");
+var ItemPriceInput=document.getElementById("item-price-input");
 var displaySalary=document.getElementById("displaySalary");
+var displayMoneySpent=document.getElementById("displayMoneySpent");
+var displayMoneyLeft=document.getElementById("displayMoneyLeft");
+
+
+// const itemArray=[
+//         {
+//             itemname:"Books",
+//             itemprice:50        
+//         }
+// ];
+
+
+
+
+
+
 
 window.onclick = function(event) {
     if (event.target == modalSalary){
         modalSalary.style.display = "none";
+        SalaryInput.value="";
     }
     else if (event.target== modalItem) {
       modalItem.style.display = "none";
+      ItemInput.value="";
+      ItemPriceInput.value="";
     }
   }
 
@@ -25,6 +45,7 @@ function removeModalSalary(){
 function removeModalItem(){
     modalItem.style.display = "none";
     ItemInput.value="";
+    ItemPriceInput.value="";
 }
 
 function AddSalary(){
@@ -43,4 +64,26 @@ function SalaryToDisplay(){
     displaySalary.innerText=getSalaryvalue;
     modalSalary.style.display = "none";
 
+}
+
+function ItemToDisplay(){
+    const getItemName=ItemInput.value;
+    const getItemPrice= parseInt(ItemPriceInput.value);
+    const getSalaryvalue= parseInt(SalaryInput.value);
+    const ItemBoxContainer=document.querySelector(".item-box-container");
+    if(getSalaryvalue > getItemPrice){
+
+        ItemBoxContainer.innerHTML= `<div class="item-name">
+        <h5 id="name">${getItemName}</h5>
+        <h5 id="item-price">Rs ${getItemPrice}</h5>
+        <i id="bin-icon"class="fas fa-trash-alt"></i>
+        </div>`
+        displayMoneySpent.innerText=getItemPrice;
+        displayMoneyLeft.innerText=getSalaryvalue-getItemPrice;
+    }else{
+        alert("You Dont Have Enough Salary to Buy Item")
+    }
+    
+    
+    modalItem.style.display="none";
 }
